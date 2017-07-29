9b382c00e4613c291b856bf802cf5b0ae5f4338b
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2017-07-29 17:06:34 +0300

    Fixes the language selector for the completion provider. Fixes #19

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,10 @@ import * as vscode from 'vscode';
 import {PathAutocomplete} from './features/PathAutocompleteProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('*', new PathAutocomplete(), '/'));
+    var selector : vscode.DocumentSelector = [{
+        pattern: '**'
+    }];
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(selector, new PathAutocomplete(), '/'));
 }
 
 // this method is called when your extension is deactivated
