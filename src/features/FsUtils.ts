6f9be943d38f41cdaedb8df9c3a7ad2b36da0c6f
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-03-13 14:50:36 +0200

    Fixes the extension loading on remote ssh #108

--- src/features/FsUtils.ts
+++ src/features/FsUtils.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import fsAsync from 'fs/promises';
-import path from 'path';
 
 export function pathExists(localPath: string) {
     return fsAsync
@@ -9,15 +8,31 @@ export function pathExists(localPath: string) {
         .catch(() => false);
 }
 
-// using original-fs rather than fs to deal with .asar file
-// ref: https://github.com/microsoft/vscode/issues/143393#issuecomment-1047518447
-const originalFs = require('original-fs') as typeof fs;
+const originalFs = getFsModule();
+
 export async function isDirectory(filePath: string): Promise<boolean> {
-    const ext = path.extname(filePath);
     return new Promise((resolve, reject) => {
         originalFs.stat(filePath, (err, statInfo) => {
-            if (err) reject(err);
+            if (err) {
+                reject(err);
+                return;
+            }
+
             resolve(statInfo.isDirectory());
         });
     });
+}
+
+function getFsModule() {
+    try {
+        // throws an error if module is not found (remote ssh environment)
+        require.resolve("original-fs");
+
+        // using original-fs rather than fs to deal with .asar file
+        // ref: https://github.com/microsoft/vscode/issues/143393#issuecomment-1047518447
+        return require('original-fs') as typeof fs;
+    } catch (e) {
+        console.log("original-fs not found, falling back to the default fs module");
+        return fs;
+    }
 }
